ba78490565c2a9ff88015f3502b70f76301865dc
Leonardo <lfanzeres@verbio.com>
2023-01-17 18:04:54 +0100

    Push existing project to GitLab

---
+++ format_number.py
@@ -0,0 +1,3 @@
+
+def format_number(number_text):
+    return 1021
---
+++ test_format_number.py
@@ -0,0 +1,12 @@
+import pytest
+from format_number import format_number
+
+number_format_dict = {'mil veintiuno':1021}
+
+@pytest.fixture
+def number_text():
+    return 'mil veintiuno'
+
+def test_format_number(number_text):
+    assert format_number(number_text) == number_format_dict.get(number_text)
+
