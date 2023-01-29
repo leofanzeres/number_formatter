7474bdd70fed7b9d3c99de48c2e6118157b6eb88
Leonardo <lfanzeres@verbio.com>
2023-01-29 06:17:07 +0100

    Added formatting feature until 6 digits number and their corresponding test functions.

--- numberformatter.py
+++ numberformatter.py
@@ -3,33 +3,82 @@ import csv
 class NumberFormatter:
     def __init__(self, numbers_dict_csv):
         self.numbers_dict_csv = numbers_dict_csv
+        self.numbers_dict = self.load_dictionary()
 
     def load_dictionary(self):
         reader = csv.reader(open(self.numbers_dict_csv, mode='r'), delimiter=',')
         numbers_dict = {}
         for row in reader:
-            numbers_dict[row[1]] = row[0]
+            zeroes = '0'*int(row[1]) if int(row[1])>0 else ''
+            numbers_dict[row[2]] = row[0] + zeroes
         return numbers_dict
 
-    def format_numbers(self, text):
-        with open(text) as f:
-            lines = f.readlines()
+    def format_numbers(self, numbers_tuple):
         formatted_lines = []
-        for l in lines:
-            formatted_lines.append(self.format_number(l))
+        for number in numbers_tuple:
+            formatted_lines.append(self.format_number(number))
         return formatted_lines
 
     def format_number(self, number):
-        numbers_dict = self.load_dictionary()
+        mil_pos = -1
         number_split = number.split(' ')
+        while 'y' in number_split: number_split.remove('y')
+
         if len(number_split) == 0:
             print('Empty string provided.')
-            formatted_number = None
-        elif len(number_split) == 1:
-            formatted_number = numbers_dict.get(number_split[0])
+            return None
+        if 'mil' in number_split:
+            mil_pos = len(number_split) - 1 - number_split[::-1].index('mil')
+            if len(number_split[mil_pos+1:]) > 0:
+                cent = self.format_up_to_3_digits(number_split[mil_pos+1:])
+            else:
+                cent = ''
+            if len(number_split[:mil_pos+1]) > 0:
+                mil = self.format_more_than_3_digits(number_split[:mil_pos+1])
+            else:
+                mil = ''
+            if len(cent) > 0:
+                formatted_number = mil[:-len(cent)] + cent
+            else:
+                formatted_number = mil
+        else:
+            formatted_number = self.format_up_to_3_digits(number_split)
+            
+        return formatted_number
+
+    def format_up_to_3_digits(self, number_split):
+        numbers = []
+        if len(number_split) == 1:
+            formatted_number = self.numbers_dict.get(number_split[0])
         elif len(number_split) == 2:
-            # TODO
+            for number in number_split:
+                numbers.append(self.numbers_dict.get(number))
+            formatted_number = numbers[0][:-len(numbers[1])] + numbers[1]
+        elif len(number_split) == 3:
+            for number in number_split:
+                numbers.append(self.numbers_dict.get(number))
+            formatted_number = numbers[0][:-len(numbers[1])] + numbers[1][:-len(numbers[2])] + numbers[2]
+        else:
+            print('Number not supported.')
             formatted_number = None
+        return formatted_number
+    
+    def format_more_than_3_digits(self, number_split):
+        numbers = []
+        if len(number_split) == 1:
+            formatted_number = self.numbers_dict.get(number_split[0])
+        elif len(number_split) == 2:
+            for number in number_split:
+                numbers.append(self.numbers_dict.get(number))
+            formatted_number = numbers[0] + numbers[1][1:]
+        elif len(number_split) == 3:
+            for number in number_split:
+                numbers.append(self.numbers_dict.get(number))
+            formatted_number = numbers[0][:-len(numbers[1])] + numbers[1] + numbers[2][1:]
+        elif len(number_split) == 4:
+            for number in number_split:
+                numbers.append(self.numbers_dict.get(number))
+            formatted_number = numbers[0][:-len(numbers[1])] + numbers[1][:-len(numbers[2])]+ numbers[2] + numbers[3][1:]
         else:
             print('Number not supported.')
             formatted_number = None
--- test/test_format_number.py
+++ test/test_format_number.py
@@ -5,31 +5,66 @@ from numberformatter import NumberFormatter
 def formatter():
     return NumberFormatter('numbers_dictionary.csv')
 
-@pytest.fixture
-def number_1_digit():
-    return 'uno'
+def test_format_number_1_digit(formatter):
+    number_1_digit = ('uno','1')
+    formatted_number = formatter.format_number(number_1_digit[0])
+    assert formatted_number is not None
+    assert formatted_number == number_1_digit[1]
 
-@pytest.fixture
-def numbers_1_digit():
-    return 'test/numbers_list_1_digit.txt'
-
-@pytest.fixture
-def number_2_digits():
-    return 'diez'
-
-@pytest.fixture
-def numbers_dict_csv(formatter):
-    return 'numbers_dictionary.csv'
-
-def test_format_number_1_digit(formatter, number_1_digit):
-    assert formatter.format_number(number_1_digit) == formatter.load_dictionary().get(number_1_digit)
-
-def test_format_numbers_1_digit(formatter, numbers_1_digit):
+def test_format_numbers_1_digit(formatter):
+    numbers_1_digit = ('cero','uno','una','un','cinco','nueve')
+    numbers_1_digit_target = ('0','1','1','1','5','9')
     formatted_numbers = formatter.format_numbers(numbers_1_digit)
-    with open(numbers_1_digit) as f:
-        lines = f.readlines()
-    dict = formatter.load_dictionary()
-    for i, number in enumerate(formatted_numbers):
-        assert number == dict.get(lines[i])
+    for i, formatted_number in enumerate(formatted_numbers):
+        assert formatted_number is not None
+        assert formatted_number == numbers_1_digit_target[i]
 
+def test_format_number_2_digits(formatter):
+    number_2_digits = ('treinta','30')
+    formatted_number = formatter.format_number(number_2_digits[0])
+    assert formatted_number is not None
+    assert formatted_number == number_2_digits[1]
 
+def test_format_numbers_2_digits(formatter):
+    numbers_1_digit = ('diez','once','veinte','veintiuno','veinticinco','treinta y uno','cuarenta y una','noventa y nueve')
+    numbers_1_digit_target = ('10','11','20','21','25','31','41','99')
+    formatted_numbers = formatter.format_numbers(numbers_1_digit)
+    for i, formatted_number in enumerate(formatted_numbers):
+        assert formatted_number is not None
+        assert formatted_number == numbers_1_digit_target[i]
+
+def test_format_numbers_3_digits(formatter):
+    numbers_1_digit = ('cien','ciento un','ciento uno','ciento una','ciento dieciocho','doscientos diez','trescientos cincuenta y ocho',
+    'quinientos treinta y una')
+    numbers_1_digit_target = ('100','101','101','101','118','210','358','531')
+    formatted_numbers = formatter.format_numbers(numbers_1_digit)
+    for i, formatted_number in enumerate(formatted_numbers):
+        assert formatted_number is not None
+        assert formatted_number == numbers_1_digit_target[i]
+
+def test_format_numbers_4_digits(formatter):
+    numbers_1_digit = ('mil','mil y uno','dos mil','mil cincuenta y siete','mil cuatrocientos sesenta y dos','mil novecientos ochenta','tres mil treinta',
+    'cinco mil setecientos cuarenta','seis mil doscientos treinta y cuatro')
+    numbers_1_digit_target = ('1000','1001','2000','1057','1462','1980','3030','5740','6234')
+    formatted_numbers = formatter.format_numbers(numbers_1_digit)
+    for i, formatted_number in enumerate(formatted_numbers):
+        assert formatted_number is not None
+        assert formatted_number == numbers_1_digit_target[i]
+
+def test_format_numbers_5_digits(formatter):
+    numbers_1_digit = ('diez mil','diez mil y uno','diez mil ochenta y uno','veinte mil cuatrocientos sesenta y dos',
+    'treinta y cuatro mil doscientos noventa y nueve','cincuenta y dos mil','cincuenta y tres mil y dos','cincuenta y cuatro mil treinta y cuatro')
+    numbers_1_digit_target = ('10000','10001','10081','20462','34299','52000','53002','54034')
+    formatted_numbers = formatter.format_numbers(numbers_1_digit)
+    for i, formatted_number in enumerate(formatted_numbers):
+        assert formatted_number is not None
+        assert formatted_number == numbers_1_digit_target[i]
+
+def test_format_numbers_6_digits(formatter):
+    numbers_1_digit = ('doscientos mil','trescientos mil y uno','cuatrocientos mil ochenta y uno','cuatrocientos treinta y tres mil doscientos',
+    'quinientos treinta y un mil cuatrocientos sesenta y cinco')
+    numbers_1_digit_target = ('200000','300001','400081','433200','531465')
+    formatted_numbers = formatter.format_numbers(numbers_1_digit)
+    for i, formatted_number in enumerate(formatted_numbers):
+        assert formatted_number is not None
+        assert formatted_number == numbers_1_digit_target[i]
